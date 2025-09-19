2795a6a9f04b1c0049e8b338d66def2bcb53c5d0
hadeel-halliq <hadeelhalliq@gmail.com>
2025-09-18 23:31:06 -0700

    add QueryClientProvider

--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,19 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { BrowserRouter as Router } from "react-router-dom";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import "./index.css";
 import App from "./App.jsx";
-import './i18n';
+import "./i18n";
+
+const queryClient = new QueryClient();
 
 createRoot(document.getElementById("root")).render(
   <Router>
     <StrictMode>
-      <App />
+      <QueryClientProvider client={queryClient}>
+        <App />
+      </QueryClientProvider>
     </StrictMode>
   </Router>
 );
